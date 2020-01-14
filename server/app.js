70ea2f9f0df43045b6913264c83aa49d18450737
Stanley Huang <huang.stanley050@gmail.com>
2020-01-14 19:32:51 +1100

    able to return token after facebook login

--- server/app.js
+++ server/app.js
@@ -1,11 +1,11 @@
 const express = require("express");
 const path = require("path");
-
+const passport = require("passport");
 const authRouter = require("./routes/auth");
-
 const app = express();
 require("./config/githubOauth");
 require("./config/facebookOauth");
+require("./config/jwtStrategy");
 
 app.use(express.static(path.join(__dirname, "../client/build")));
 
@@ -13,7 +13,9 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use(authRouter);
-app.get("/api", (req, res) => res.send("all api routes"));
+app.get("/api", passport.authenticate("jwt", { session: false }), (req, res) =>
+  res.send("all api routes")
+);
 
 app.get("*", (req, res) => {
   res.sendFile(path.join(`${__dirname}../client/build/index.html`));
--- server/config/githubOauth.js
+++ server/config/githubOauth.js
@@ -8,7 +8,6 @@ passport.use(
       clientID: process.env.GIT_HUB_CLIENT_ID,
       clientSecret: process.GIT_HUB_CLIENT_SECRET,
       callbackURL: "/auth/github/callback",
-
       proxy: true
     },
     (token, tokenSecret, profile, done) => {
