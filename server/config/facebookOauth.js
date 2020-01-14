6c154caf74e77c211dafe974b6bed5b434d70c64
Stanley Huang <huang.stanley050@gmail.com>
2020-01-14 14:29:57 +1100

    getting user details from facebook

--- server/config/facebookOauth.js
+++ server/config/facebookOauth.js
@@ -8,6 +8,7 @@ passport.use(
       clientID: process.env.FACEBOOK_CLIENT,
       clientSecret: process.env.FACEBOOK_SECRET,
       callbackURL: "/auth/facebook/callback",
+      profileFields: ["id", "displayName", "email", "name"],
       proxy: true
     },
     (accessToken, refreshToken, profile, done) => {
