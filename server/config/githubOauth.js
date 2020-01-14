58364c9f4cb34117b607009e8d8c3a7ccb0cfcef
Stanley Huang <huang.stanley050@gmail.com>
2020-01-14 21:02:42 +1100

    getting ready to handle local login and register

--- server/config/githubOauth.js
+++ server/config/githubOauth.js
@@ -8,6 +8,7 @@ passport.use(
       clientID: process.env.GIT_HUB_CLIENT_ID,
       clientSecret: process.GIT_HUB_CLIENT_SECRET,
       callbackURL: "/auth/github/callback",
+
       proxy: true
     },
     (token, tokenSecret, profile, done) => {
