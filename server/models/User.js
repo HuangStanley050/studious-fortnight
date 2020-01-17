25ac3f633c7733cb64f9aa8311336ee29a88edfa
Stanley Huang <huang.stanley050@gmail.com>
2020-01-17 14:18:06 +1100

    remove unnecessary code in User schema

--- server/models/User.js
+++ server/models/User.js
@@ -1,49 +1,48 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-const userSchema = new mongoose.Schema(
-  {
-    password: {
-      type: String
-    },
-    email: {
-      type: String,
-      required: true
-    },
-    externalProvider: {
-      type: String
-    },
-    dailyStreak: {
-      type: Number
-    },
-    totalTimeMeditated: {
-      type: Number
-    },
-    lastMeditated: {
-      type: Date
-    },
-    badges: [{
+const userSchema = new mongoose.Schema({
+  password: {
+    type: String
+  },
+  email: {
+    type: String,
+    required: true
+  },
+  externalProvider: {
+    type: String
+  },
+  dailyStreak: {
+    type: Number
+  },
+  totalTimeMeditated: {
+    type: Number
+  },
+  lastMeditated: {
+    type: Date
+  },
+  badges: [
+    {
       name: String,
       unlocked: Boolean,
-      image: String 
-    }],
-    preferredMeditationTime: {
-      type: String
-    },
-    startingPoint: {
-      type: String
-    }, 
-    purposeOfJoining: {
-      type: String
-    },
-    activeUser: {
-      type: Boolean
-    },
-    meditationId: {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: "Meditation"
+      image: String
     }
-  }, {collection: 'user'}
-)
+  ],
+  preferredMeditationTime: {
+    type: String
+  },
+  startingPoint: {
+    type: String
+  },
+  purposeOfJoining: {
+    type: String
+  },
+  activeUser: {
+    type: Boolean
+  },
+  meditationId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Meditation"
+  }
+});
 
-
-module.exports = mongoose.model('user', userSchema);
+module.exports = mongoose.model("user", userSchema);
