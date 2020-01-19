08e74994248fae5e5f452280a86a0906f43efe37
Stanley Huang <huang.stanley050@gmail.com>
2020-01-20 10:35:00 +1100

    more fixes, route and controller in progress

--- server/routes/payment.js
+++ server/routes/payment.js
@@ -2,7 +2,6 @@ const express = require("express");
 const router = express.Router();
 const passport = require("passport");
 const PaymentController = require("../controllers/payment");
-const Donation = require("../models/Donation");
 
 router.post(
   "/api/dontation",
