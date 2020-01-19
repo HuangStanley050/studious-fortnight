16e326680885abe4dc36d16053cf7b4f1d06c00a
Stanley Huang <huang.stanley050@gmail.com>
2020-01-20 09:57:36 +1100

    refined the schemas for Donation, Meditation and User

--- server/models/User.js
+++ server/models/User.js
@@ -1,48 +1,65 @@
 const mongoose = require("mongoose");
 
-const userSchema = new mongoose.Schema({
-  password: {
-    type: String
+const userSchema = new mongoose.Schema(
+  {
+    password: {
+      type: String
+    },
+    email: {
+      type: String,
+      required: true
+    },
+    externalProvider: {
+      type: String
+    },
+    dailyStreak: {
+      type: Number
+    },
+    totalTimeMeditated: {
+      type: Number
+    },
+    lastMeditated: {
+      type: Date
+    },
+    badges: [
+      {
+        name: String,
+        unlocked: Boolean,
+        image: String
+      }
+    ],
+    preferredMeditationTime: {
+      type: String
+    },
+    startingPoint: {
+      type: String
+    },
+    purposeOfJoining: {
+      type: String
+    },
+    activeUser: {
+      type: Boolean
+    },
+    courseId: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Course"
+      }
+    ],
+    meditationId: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Meditation"
+      }
+    ],
+    donationId: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Donation"
+      }
+    ]
   },
-  email: {
-    type: String,
-    required: true
-  },
-  externalProvider: {
-    type: String
-  },
-  dailyStreak: {
-    type: Number
-  },
-  totalTimeMeditated: {
-    type: Number
-  },
-  lastMeditated: {
-    type: Date
-  },
-  badges: [
-    {
-      name: String,
-      unlocked: Boolean,
-      image: String
-    }
-  ],
-  preferredMeditationTime: {
-    type: String
-  },
-  startingPoint: {
-    type: String
-  },
-  purposeOfJoining: {
-    type: String
-  },
-  activeUser: {
-    type: Boolean
-  },
-  meditationId: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "Meditation"
-  }
-});
+  { timestamps: true }
+);
 
 module.exports = mongoose.model("user", userSchema);
