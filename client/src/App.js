12bae65f63a9b08e685b2c6dac5c8a0b1f4a1cdb
Stanley Huang <huang.stanley050@gmail.com>
2020-01-20 20:29:50 +1100

    auth page has both login and register component now

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,4 @@
 import React from "react";
-
 import { Container } from "reactstrap";
 import { Route, Switch } from "react-router-dom";
 import NavBar from "./components/NavBar.jsx";
@@ -9,6 +8,7 @@ import Discover from "./pages/Discover.jsx";
 import Profile from "./pages/Profile.jsx";
 import Contact from "./pages/Contact.jsx";
 import About from "./pages/About.jsx";
+import AuthPage from "./pages/Auth.jsx";
 import NotFoundPage from "./pages/NotFoundPage.jsx";
 
 function App() {
@@ -16,11 +16,12 @@ function App() {
     <Container>
       <NavBar />
       <Switch>
-        <Route path="/" component={Home} exact={true} />
+        <Route exact path="/" component={Home} exact={true} />
         <Route path="/discover" component={Discover} />
         <Route path="/profile/stats" component={Profile} />
         <Route path="/contact" component={Contact} />
         <Route path="/about" component={About} />
+        <Route path="/auth" component={AuthPage} />
         <Route component={NotFoundPage} />
       </Switch>
       <Footer />
---
+++ client/src/components/Login.jsx
@@ -0,0 +1,7 @@
+import React, { useState, useEffect } from "react";
+
+const Login = props => {
+  return <h1>Login component</h1>;
+};
+
+export default Login;
