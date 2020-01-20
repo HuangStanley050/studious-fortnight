b4b875d957a12688cc5a0650a96e4c6305a8eaf7
HuangStanley050 <huang.stanley050@gmail.com>
2020-01-20 18:32:55 +1100

    remove git marks
    removed
    
    >>>master

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,19 @@
 import React from "react";
 import "./App.css";
-<<<<<<< HEAD
-import Login from './pages/Login';
-=======
+
+//import Login from './pages/Login';
+
 //import PaymentForm from "./components/PaymentForm";
 //import Login from './pages/Login';
->>>>>>> master
+
 
 function App() {
   return (
     <>
       <h1>Hello world</h1>
-<<<<<<< HEAD
-      <Login />
-=======
+
       {/*<PaymentForm />*/}
->>>>>>> master
+
     </>
   );
 }
