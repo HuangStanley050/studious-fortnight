ce858a0a4fb0c5df5548e0e8f2f0c455096287d9
Stanley Huang <huang.stanley050@gmail.com>
2020-01-22 10:48:03 +1100

    protect PasswordReset page from invalid route params

--- client/src/pages/PasswordReset.jsx
+++ client/src/pages/PasswordReset.jsx
@@ -1,8 +1,18 @@
 import React, { useEffect } from "react";
+import { Redirect } from "react-router-dom";
+import jwt_decode from "jwt-decode";
 
-const PasswordReset = props => {
-  console.log(props.userId);
-  console.log(props.token);
+/*
+id.match(/^[0-9a-fA-F]{24}$/)
+ */
+const PasswordReset = ({ userId, token }) => {
+  // check if the route params is valid
+  // prevent user from getting to it directly
+  // this will only load from the link opened from user email
+  if (!userId.match(/^[0-9a-fA-F]{24}$/) || !jwt_decode(token)) {
+    console.log("not a valid userId");
+    return <Redirect to="/" />;
+  }
   return <h1>Password reset form</h1>;
 };
 
