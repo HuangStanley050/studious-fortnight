da348bbac22db0b50d8f1b45daffa5c361924125
Stanley Huang <huang.stanley050@gmail.com>
2020-01-21 19:44:21 +1100

    redirect user back to UI for password reset from email with token and userID details

---
+++ client/src/pages/PasswordReset.jsx
@@ -0,0 +1,9 @@
+import React, { useEffect } from "react";
+
+const PasswordReset = props => {
+  console.log(props.userId);
+  console.log(props.token);
+  return <h1>Password reset</h1>;
+};
+
+export default PasswordReset;
