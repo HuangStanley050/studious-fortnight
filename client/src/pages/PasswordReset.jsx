750ec7dc72435ad3e284f940b20fb95a38e3f4cb
Stanley Huang <huang.stanley050@gmail.com>
2020-01-22 11:14:22 +1100

    password reset form taking values

--- client/src/pages/PasswordReset.jsx
+++ client/src/pages/PasswordReset.jsx
@@ -1,5 +1,7 @@
-import React, { useEffect } from "react";
+import React, { useState } from "react";
 import { Redirect } from "react-router-dom";
+import API from "../api";
+import axios from "axios";
 import jwt_decode from "jwt-decode";
 
 /*
@@ -9,11 +11,49 @@ const PasswordReset = ({ userId, token }) => {
   // check if the route params is valid
   // prevent user from getting to it directly from client side
   // this will only load from the link opened from user email
-  if (!userId.match(/^[0-9a-fA-F]{24}$/) || !jwt_decode(token)) {
-    console.log("not a valid userId");
-    return <Redirect to="/" />;
-  }
-  return <h1>Password reset form</h1>;
+  const [passwordDetail, setPasswordDetail] = useState({
+    password: "",
+    confirmPassword: ""
+  });
+  const handleChange = e => {
+    setPasswordDetail({
+      ...passwordDetail,
+      [e.target.name]: e.target.value
+    });
+  };
+  // if (!userId.match(/^[0-9a-fA-F]{24}$/) || !jwt_decode(token)) {
+  //   console.log("not a valid userId");
+  //   return <Redirect to="/" />;
+  // }
+  const handleSubmit = e => {
+    e.preventDefault();
+    console.log(passwordDetail);
+  };
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <h1>Password reset form</h1>
+        <div>
+          <input
+            type="password"
+            name="password"
+            value={passwordDetail.password}
+            onChange={handleChange}
+          />
+        </div>
+        <div>
+          <input
+            type="password"
+            name="confirmPassword"
+            value={passwordDetail.confirmPassword}
+            onChange={handleChange}
+          />
+        </div>
+
+        <button type="submit">Submit</button>
+      </form>
+    </div>
+  );
 };
 
 export default PasswordReset;
