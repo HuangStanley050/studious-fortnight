2c5738d7e91056b6a6a70b8eefd2edf65c765171
Stanley Huang <huang.stanley050@gmail.com>
2020-01-23 21:00:40 +1100

    testing to see if authRoute test runs in Travis

---
+++ server/__tests__/authRoutes.test.js
@@ -0,0 +1,14 @@
+const app = require("../app"); // Link to your server file
+const supertest = require("supertest");
+const request = supertest(app);
+
+test("Local login should fail with incorrect password", async done => {
+  const res = await request.post("/api/auth/local/login").send({
+    email: "test@test.com",
+    password: "password2222"
+  });
+  //console.log(res);
+  expect(res.text).toBe("Unable to login");
+  expect(res.statusCode).toBe(400);
+  done();
+});
