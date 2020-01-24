53c3e4123fba0dcd66146e9ab11b82cfb39fa8d6
Stanley Huang <huang.stanley050@gmail.com>
2020-01-24 14:09:08 +1100

    added some test cases for auth routes in api

--- server/__tests__/authRoutes.test.js
+++ server/__tests__/authRoutes.test.js
@@ -14,3 +14,42 @@ test("Local login should fail with incorrect password", async done => {
   //mongoose.connection.close();
   done();
 });
+
+test("Local login should work with right password", async done => {
+  const res = await request.post("/api/auth/local/login").send({
+    email: "test@test.com",
+    password: "password"
+  });
+  //console.log(res);
+  //expect(res.text).toBe("Password is not correct");
+  expect(res.statusCode).toBe(200);
+  //mongoose.connection.close();
+  done();
+});
+
+test("Local register should fail with invalid email", async done => {
+  const res = await request.post("/api/auth/local/register").send({
+    email: "asdfadsf@dddd",
+    password: "asdfasdf"
+  });
+  expect(res.statusCode).toBe(400);
+  done();
+});
+
+test("Local register should fail with invalid password", async done => {
+  const res = await request.post("/api/auth/local/register").send({
+    email: "asdfadsf@dddd.com",
+    password: "asdfasdf"
+  });
+  expect(res.statusCode).toBe(400);
+  done();
+});
+
+test("Local register should be okay with valid password", async done => {
+  const res = await request.post("/api/auth/local/register").send({
+    email: "asdfadsf@dddd.com",
+    password: "asdfasdf1A@ddsaf"
+  });
+  expect(res.statusCode).toBe(200);
+  done();
+});
