7a0118cc36418864036df4c7c087fd55fcecc008
Stanley Huang <huang.stanley050@gmail.com>
2020-01-28 10:34:53 +1100

    page 4 done

--- client/src/components/PageFour.jsx
+++ client/src/components/PageFour.jsx
@@ -5,6 +5,10 @@ import axios from "axios";
 import API from "../api";
 import { closeQuiz } from "../store/actions/quizActions";
 import { Field, reduxForm, getFormValues } from "redux-form";
+import morningPic from "../assets/morning.jpeg";
+import noonPic from "../assets/noon.jpg";
+import nightPic from "../assets/night.jpeg";
+import { Container, Col } from "reactstrap";
 
 let PageFour = ({ values, prevPage, dispatch }) => {
   const submitHandler = async e => {
@@ -28,40 +32,103 @@ let PageFour = ({ values, prevPage, dispatch }) => {
     console.log(result.data);
     dispatch(closeQuiz());
   };
+  let selection;
+  const morning = "morning";
+  const afternoon = "afternoon";
+  const night = "night";
+  const selectionStyle = {
+    boxShadow: "0 0 5px 5px #2ecc71"
+  };
+  if (values) {
+    selection = values.time;
+  }
   return (
-    <form onSubmit={submitHandler}>
-      <h1>Page Four</h1>
-      <h2>Preferred Meditation Time</h2>
-      <div>
-        <div>
-          <label>
-            <Field name="time" component="input" type="radio" value="morning" />
-            Morning
-          </label>
-        </div>
-        <div>
-          <label>
+    <Container>
+      <h2 style={{ textAlign: "center" }}>Preferred Meditation Time</h2>
+      <form className="row">
+        <Col sm="4" md="4" lg="4">
+          <label className="special-label">
             <Field
+              className="card-input-element"
+              name="time"
+              component="input"
+              type="radio"
+              value="morning"
+            />
+            <div
+              className="card card-input"
+              style={selection === morning ? selectionStyle : null}
+            >
+              <div className="card-body">
+                <img
+                  src={morningPic}
+                  className="card-image-top"
+                  alt="morning"
+                  style={{ width: "278px", height: "259px" }}
+                />
+                <div className="card-title">Morning</div>
+              </div>
+            </div>
+          </label>
+        </Col>
+        <Col sm="4" md="4" lg="4">
+          <label className="special-label">
+            <Field
+              className="card-input-element"
               name="time"
               component="input"
               type="radio"
               value="afternoon"
             />
-            Afternoon
+            <div
+              className="card card-input"
+              style={selection === afternoon ? selectionStyle : null}
+            >
+              <div className="card-body">
+                <img
+                  src={noonPic}
+                  className="card-image-top"
+                  alt="morning"
+                  style={{ width: "278px", height: "259px" }}
+                />
+                <div className="card-title">Afternoon</div>
+              </div>
+            </div>
           </label>
-        </div>
+        </Col>
+        <Col sm="4" md="4" lg="4">
+          <label className="special-label">
+            <Field
+              className="card-input-element"
+              name="time"
+              component="input"
+              type="radio"
+              value="night"
+            />
+            <div
+              className="card card-input"
+              style={selection === night ? selectionStyle : null}
+            >
+              <div className="card-body">
+                <img
+                  src={nightPic}
+                  className="card-image-top"
+                  alt="morning"
+                  style={{ width: "278px", height: "259px" }}
+                />
+                <div className="card-title">Night</div>
+              </div>
+            </div>
+          </label>
+        </Col>
         <div>
-          <label>
-            <Field name="time" component="input" type="radio" value="night" />
-            Night
-          </label>
+          <button type="button" className="previous" onClick={prevPage}>
+            Previous
+          </button>
+          <button>Submit</button>
         </div>
-      </div>
-      <button type="button" className="previous" onClick={prevPage}>
-        Previous
-      </button>
-      <button>Submit</button>
-    </form>
+      </form>
+    </Container>
   );
 };
 
@@ -73,3 +140,39 @@ export default reduxForm({
   destroyOnUnmount: false, // <------ preserve form data
   forceUnregisterOnUnmount: true // <------ unregister fields on unmount
 })(PageFour);
+
+/*
+<form onSubmit={submitHandler}>
+  <h1>Page Four</h1>
+  <h2>Preferred Meditation Time</h2>
+  <div>
+    <div>
+      <label>
+        <Field name="time" component="input" type="radio" value="morning" />
+        Morning
+      </label>
+    </div>
+    <div>
+      <label>
+        <Field
+          name="time"
+          component="input"
+          type="radio"
+          value="afternoon"
+        />
+        Afternoon
+      </label>
+    </div>
+    <div>
+      <label>
+        <Field name="time" component="input" type="radio" value="night" />
+        Night
+      </label>
+    </div>
+  </div>
+  <button type="button" className="previous" onClick={prevPage}>
+    Previous
+  </button>
+  <button>Submit</button>
+</form>
+ */
