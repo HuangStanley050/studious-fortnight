ef7c8cb17e13cc3a07c7abfc068001a788f0eba4
Stanley Huang <huang.stanley050@gmail.com>
2020-01-28 14:33:41 +1100

    implemented logout at Profile page

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -31,19 +31,18 @@ const Home = ({ hasRegistered }) => {
   //console.log("currnetMeditation data: ", currentMeditation);
   return (
     <>
-
-      {currentMeditation.sessionDetail != undefined ?   
-        <> 
-        <h1>Home page</h1>
-        <p>Current meditation: {currentMeditation._id}</p>
-        <p>Completed: {currentMeditation.completed ? "true" : "false" }</p>
-        <p>Time: {currentMeditation.sessionDetail.totalTime}</p>
-        <p>level: {currentMeditation.sessionDetail.level}</p>
+      {currentMeditation.sessionDetail != undefined ? (
+        <>
+          <h1>Home page</h1>
+          <p>Current meditation: {currentMeditation._id}</p>
+          <p>Completed: {currentMeditation.completed ? "true" : "false"}</p>
+          <p>Time: {currentMeditation.sessionDetail.totalTime}</p>
+          <p>level: {currentMeditation.sessionDetail.level}</p>
         </>
-      :
+      ) : (
         <p> hmm</p>
-      }
-      <h4>{error ? errorMsg : null}</h4>
+      )}
+      {error ? errorMsg : null}
       {hasRegistered ? <Quiz /> : null}
     </>
   );
