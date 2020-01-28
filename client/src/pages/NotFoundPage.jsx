d667ca318ac2eac641c74f5f5eea469c598bffde
Stanley Huang <huang.stanley050@gmail.com>
2020-01-28 14:42:30 +1100

    styled 404 page

--- client/src/pages/NotFoundPage.jsx
+++ client/src/pages/NotFoundPage.jsx
@@ -1,12 +1,23 @@
-import React from 'react';
-import {Link} from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
+import "./NotFoundPage.css";
 
 const NotFoundPage = () => {
   return (
-    <>
-      <h1>404 Not found - <Link to="/">Go Home</Link></h1>
-    </>
-  )
-}
+    <div id="notfound">
+      <div class="notfound">
+        <div class="notfound-404">
+          <h1>404</h1>
+        </div>
+        <h2>We are sorry, Page not found!</h2>
+        <p>
+          The page you are looking for might have been removed had its name
+          changed or is temporarily unavailable.
+        </p>
+        <Link to="/">Back To Homepage</Link>
+      </div>
+    </div>
+  );
+};
 
 export default NotFoundPage;
