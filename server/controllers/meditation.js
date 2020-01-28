2b3b28ccfdb52548218f22aa99c463905d986104
Stanley Huang <huang.stanley050@gmail.com>
2020-01-28 12:45:11 +1100

    error message shows if user has no currentMeditation set

--- server/controllers/meditation.js
+++ server/controllers/meditation.js
@@ -7,11 +7,19 @@ exports.returnUserMeditation = async (req, res) => {
   // const {id} = req.body;
   // console.log("id ==>", id);
   //console.log("returning user meditation......from returnUserMeditation");
-  const result = await User.findOne({ _id: id });
-  //console.log("result of finding the meditation from user: ", result);
-  const meditationId = result.currentMeditation;
+  try {
+    const result = await User.findOne({ _id: id });
+    //console.log("result of finding the meditation from user: ", result);
+    if (!result.currentMeditation) {
+      throw new Error("user has no existing meditation");
+    }
+    const meditationId = result.currentMeditation;
 
-  let meditation = await Meditation.findOne({ _id: meditationId });
-
-  return res.send(meditation);
+    let meditation = await Meditation.findOne({ _id: meditationId });
+    //console.log(meditation);
+    return res.send(meditation);
+  } catch (err) {
+    console.log("no current Meditation");
+    return res.status(500).send({ msg: "Unable to find current meditation" });
+  }
 };
