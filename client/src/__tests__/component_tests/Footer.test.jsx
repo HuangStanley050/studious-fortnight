5c3cd9f9ddf0d0a96d2336938f667f487d06cc6f
Mark <markke4@outlook.com>
2020-02-04 10:21:24 +1100

    testing

--- client/src/__tests__/component_tests/Footer.test.jsx
+++ client/src/__tests__/component_tests/Footer.test.jsx
@@ -1,10 +1,9 @@
-import Footer from "../components/Footer";
+import Footer from "../../components/Footer";
 import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import { render } from "@testing-library/react";
 
 test("<Footer /> should render 'contact and 'about'", () => {
-  //https://create-react-app.dev/docs/running-tests/
   const testComponent = (
     <Router>
       <Route component={Footer} />
