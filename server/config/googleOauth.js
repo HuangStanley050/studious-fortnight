59bd2b6b6fb5a6453ca5dcf4c03a32559ff2621f
Stanley Huang <huang.stanley050@gmail.com>
2020-02-05 10:09:54 +1100

    fixing some issue with oauth routes on the production

--- server/config/googleOauth.js
+++ server/config/googleOauth.js
@@ -9,7 +9,7 @@ passport.use(
     {
       clientID: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      callbackURL: "/api/auth/google/callback",
+      callbackURL: "/auth/google/callback",
       proxy: true
     },
     async (accessToken, refreshToken, profile, done) => {
