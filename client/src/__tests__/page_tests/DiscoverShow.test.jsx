a5b89b8f51bbc2078f5a53e5c09aea36b6ac6806
Stanley Huang <huang.stanley050@gmail.com>
2020-02-05 09:32:40 +1100

    fixed discover show page test

--- client/src/__tests__/page_tests/DiscoverShow.test.jsx
+++ client/src/__tests__/page_tests/DiscoverShow.test.jsx
@@ -2,27 +2,22 @@ import DiscoverShow from "../../pages/DiscoverShow";
 import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import { render, fireEvent } from "@testing-library/react";
-import {Provider} from "react-redux";
-
+import { Provider } from "react-redux";
 
 //having trouble getting props.match.params ?? Saying its null...
 
-const props = {match: {params: {id: 1}}};
-test("<DiscoverShow /> should render 'About CMCflow'", () => {
-  const store = {getState: () => {}, subscribe: () => {}}
+const props = { match: { params: { id: "1" } } };
+test("<DiscoverShow /> should render 'Beginner'", () => {
+  const store = { getState: () => {}, subscribe: () => {} };
   const testComponent = (
     <Provider store={store}>
       <Router>
-        <Route render={() => (
-                <DiscoverShow
-                  props={props}
-                />
-        )} />
+        <Route render={() => <DiscoverShow {...props} />} />
       </Router>
     </Provider>
   );
 
   console.log(props);
   const { getByText } = render(testComponent);
-  expect(getByText("About CMCflow")).toBeInTheDocument();
+  expect(getByText("Beginner")).toBeInTheDocument();
 });
--- client/src/__tests__/page_tests/Home.test.jsx
+++ client/src/__tests__/page_tests/Home.test.jsx
@@ -1,24 +1,23 @@
-import {Home} from "../../pages/home";
+import { Home } from "../../pages/Home";
 import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import { render, fireEvent } from "@testing-library/react";
-import {Provider} from "react-redux";
+import { Provider } from "react-redux";
 
-const store = {getState: () => {}, subscribe: () => {}, dispatch: () => {}}
+const store = { getState: () => {}, subscribe: () => {}, dispatch: () => {} };
 const meditationSession = {
-  sessionDetail: {level: 1, totalTime: 180}
+  sessionDetail: { level: 1, totalTime: 180 }
 };
 
 test("<Home /> should render 'Level 1'", () => {
   const testComponent = (
     <Provider store={store}>
       <Router>
-        <Route render={(user) => (
-            <Home
-              user={user}
-              meditationSession={meditationSession}
-            />
-        )} />
+        <Route
+          render={user => (
+            <Home user={user} meditationSession={meditationSession} />
+          )}
+        />
       </Router>
     </Provider>
   );
@@ -31,12 +30,11 @@ test("<Home /> should render 'Lets start your meditation session.'", () => {
   const testComponent = (
     <Provider store={store}>
       <Router>
-        <Route render={(user) => (
-            <Home
-              user={user}
-              meditationSession={meditationSession}
-            />
-        )} />
+        <Route
+          render={user => (
+            <Home user={user} meditationSession={meditationSession} />
+          )}
+        />
       </Router>
     </Provider>
   );
