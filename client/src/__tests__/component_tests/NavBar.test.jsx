9c8e17a6c3907b646e54d59b0170b53c7e670e81
Carl Davidson <carl.davidson@hotmail.com>
2020-02-04 17:32:13 +1100

    working on logout

--- client/src/__tests__/component_tests/NavBar.test.jsx
+++ client/src/__tests__/component_tests/NavBar.test.jsx
@@ -2,12 +2,10 @@ import { NavBar } from "../../components/NavBar.jsx";
 import Logout from "../../components/Logout.jsx";
 import React from "react";
 import { BrowserRouter, Route, Router, NavLink } from "react-router-dom";
-import { connect, Provider } from "react-redux";
+
 import { render } from "@testing-library/react";
-// import DeactivateAccount from "../../components/DeactivateAccount.jsx";
-const store = {getState: () => {}, subscribe: () => {}}
-const user = {email: "carl@carl.com"}
-const isAuth = true;
+
+
 
 test("<Navbar /> should render 'Login'", () => {
   const testComponent = (
