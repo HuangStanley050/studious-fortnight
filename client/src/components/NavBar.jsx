82fb920f92c0275d77bffcb3c8625e86acf2f610
Mark <markke4@outlook.com>
2020-02-06 10:49:46 +1100

    phone responsive media ueries

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
 import { connect } from "react-redux";
-import "./NavBar.scss";
+import "./NavBar.css";
 import LOGO from "../assets/Logo1.png"
 
 
